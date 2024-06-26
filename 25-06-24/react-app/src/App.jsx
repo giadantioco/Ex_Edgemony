68057c0cd12c1d3e21737acf1aeeade53569fb62
giadantioco <giada.antioco@gmail.com>
2024-06-26 09:42:31 +0200

    26-06-24

--- 25-06-24/react-app/src/App.jsx
+++ 25-06-24/react-app/src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, Fragment } from "react"; // Fragment is a component tag, it can also be empty <></>
+import "./App.css"; // this is possible because we're using Vite --> CSS inline in HTML
+import { Header } from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <Fragment>
+      <Header />
+      <div className="container">
+        <h1>CB10</h1>
+
+        <div className="card">
+          <button onClick={() => console.log("ciao")}>Click me!</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Fragment>
   );
 }
 
--- 25-06-24/react-app/src/main.jsx
+++ 25-06-24/react-app/src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react"; // line 13 package.json
-import ReactDOM from "react-dom/client"; // line 14 package.json
+import ReactDOM from "react-dom/client"; // line 14 package.json --> React is a library created to handle JSX. By reactDOM jsx communicates with the DOM
 import App from "./App.jsx"; // line 6 App.jsx
-import "./index.css";
+import "./index.css"; // CSS import is possible thanks to Vite (inline CSS)
 
+// createRoot creates react application on selected element
 const element = document.getElementById("root");
 ReactDOM.createRoot(element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// createRoot creates react application on selected element
