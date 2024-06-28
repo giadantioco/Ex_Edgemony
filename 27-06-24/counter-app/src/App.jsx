05465ed6a90fe0be05d05a3cb4f3768fd30476ba
giadantioco <giada.antioco@gmail.com>
2024-06-28 09:42:01 +0200

    feat: fixed UI

--- 27-06-24/counter-app/src/App.jsx
+++ 27-06-24/counter-app/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+// components imports
 import { Button } from "./components/atoms/Button/Button";
 import { Counter } from "./components/components/Counter/Counter";
+// icon imports
+import iconCss from "./assets/css.png";
+import iconHtml from "./assets/html5.png";
+import iconJs from "./assets/js.png";
 import "./App.css";
 
 function App() {
@@ -9,6 +14,11 @@ function App() {
       <h1 className="title">Javasctipt Project</h1>
       <div className="counter-container">
         <Counter />
+      </div>
+      <div className="img-container">
+        <img src={iconCss} alt="css logo" style={{ width: "100px" }} />
+        <img src={iconHtml} alt="html logo" style={{ width: "100px" }} />
+        <img src={iconJs} alt="js logo" style={{ width: "100px" }} />
       </div>
     </>
   );
