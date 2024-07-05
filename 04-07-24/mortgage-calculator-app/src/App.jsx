fa0afa37975bbdc9c1a5b0e3b33dde34714c45ad
giadantioco <giada.antioco@gmail.com>
2024-07-05 09:09:19 +0200

    logic added

--- 04-07-24/mortgage-calculator-app/src/App.jsx
+++ 04-07-24/mortgage-calculator-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // assigning keys to object state
   const [input, setInput] = useState({
     loanAmount: "",
     interestRate: "",
@@ -9,8 +10,7 @@ function App() {
     mortgageType: "Repayment",
   });
 
-  const [montlyPayment, setMontlyPayment] = useState(" £0.00");
-
+  // taking key values,save them in a variable and save input in prevstate on event change, necessary on onChange events
   const handleInputChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -20,11 +20,37 @@ function App() {
     setInput((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  // create states for totals
+  const [montlyPayment, setMontlyPayment] = useState(" £0.00");
+  const [totalPayment, setTotalPayment] = useState("£0.00");
+
+  // calculate monthly payment
   const handleMonthlyPayment = (e) => {
     e.preventDefault();
-    console.log("ciao");
-    const total = input.loanAmount * input.loanTerm;
+    console.log(e);
+    const total = Math.floor(
+      (input.loanAmount * input.loanTerm) / input.interestRate
+    );
     setMontlyPayment(total);
+    console.log({ "monthly payment": total });
+  };
+
+  // calculte total payment
+  const handleTotalPayment = (e) => {
+    e.preventDefault();
+    console.log(e);
+    const total = Math.floor(
+      input.loanAmount * (1 + (input.interestRate / 100) * input.loanTerm)
+    );
+    setTotalPayment(total);
+    console.log({ "total payment": total });
+  };
+
+  //create function that handles both functions for paymnent to pass to the button, as it can accept only one function a time
+
+  const handleClick = (e) => {
+    handleMonthlyPayment(e);
+    handleTotalPayment(e);
   };
 
   return (
@@ -90,14 +116,17 @@ function App() {
             </div>
           </div>
           <div className="form-group">
-            <button type="submit" onClick={handleMonthlyPayment}>
+            <button type="submit" onClick={handleClick}>
               Calculate
             </button>
           </div>
         </form>
         <div className="result">
           <p>
-            Monthly Payment: <span id="monthly-payment">{montlyPayment}</span>
+            Monthly Payment: £<span id="monthly-payment">{montlyPayment}</span>
+          </p>
+          <p>
+            Total Payment: £<span id="total-payment">{totalPayment}</span>
           </p>
         </div>
       </div>
