3000c8ab4df085dedabce904ccc2e1a0a7832b53
giadantioco <giada.antioco@gmail.com>
2024-07-17 18:25:05 +0200

    feat: add add-to-cart logic

--- 17-07-24/react-router/src/App.jsx
+++ 17-07-24/react-router/src/App.jsx
@@ -3,16 +3,34 @@ import { useEffect, useState } from "react";
 function App() {
   const [products, setProducts] = useState([]);
 
+  // pulisci li local storage prima della fetch
+  localStorage.clear();
+
   useEffect(() => {
     fetch("https://api.escuelajs.co/api/v1/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.slice(0, 10)));
+    console.log(products);
   }, []);
 
-  // useEffect(() => {
-  //   if (!products) return;
-  //   localStorage.setItem("user", JSON.stringify(products));
-  // }, [products]);
+  // useEffect per 'loggare' prodotti dopo primo render
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
+  //  funzione che aggiunge prodotti al carrello
+  const addToCart = (product) => {
+    // tentativo di recupero del prodotto corrente dal local storage
+    // se il carrello è vuoto restituisce null, ma con || [] evitoche questo accada
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    // debugging
+    console.log(localStorage);
+    console.log(localStorage.getItem("cart"));
+    console.log(cart);
+    // il prodotto selezionato viene aggiunto all'array 'cart'
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
 
   return (
     <>
@@ -22,6 +40,7 @@ function App() {
           {products.map((product) => (
             <li key={product.id}>
               <p>{product.title}</p>
+              <img src={product.images[0]} alt="" />
               <button onClick={() => addToCart(product)}>Add to Cart</button>
             </li>
           ))}
