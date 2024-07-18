7ef9a87e862f192d4933a644b4be7f3f1931bd1f
giadantioco <giada.antioco@gmail.com>
2024-07-18 17:07:26 +0200

    feat: add dynamic API call

--- 18-07-24/react-e-commerce/src/pages/ProductPage.jsx
+++ 18-07-24/react-e-commerce/src/pages/ProductPage.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function ProductPage() {
-  // destructuring del parametro id
+  // destructuring del parametro id con use parms, che c permette di andare aprendere le informazioni specificate per visualizzarle nel path
   const { id } = useParams();
   console.log(useParams());
-  return <div>Ciao sono page id: {id}</div>;
+
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.escuelajs.co/api/v1/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+    console.log(product);
+  }, []);
+
+  return (
+    <div>
+      {/* la condizione product && product.title va messa sempre perche a primo render product risulta null (come dichiarato nel useState) quindi ci darebbe errore */}
+      Ciao sono prodotto id: {id} {product && product.title}
+    </div>
+  );
 }
 
 export default ProductPage;
