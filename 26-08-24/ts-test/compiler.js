6c459953f0361cfd185e7f623445f66bb7eee2d2
giadantioco <giada.antioco@gmail.com>
2024-08-26 19:34:37 +0200

    add lexical analysis

--- 26-08-24/ts-test/compiler.js
+++ 26-08-24/ts-test/compiler.js
@@ -33,12 +33,49 @@ function tokenizer(input) {
       current++;
       continue;
     }
+    // we don't save whute spaces into tokens, we don't need to; we just check if they exist
     let WHITESPACE = /\s/;
     if (WHITESPACE.test(char)) {
       current++;
       continue;
     }
+
+    let NUMBERS = /[0-9]/;
+    if (NUMBERS.test(char)) {
+      let value = "";
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+      tokens.push({ type: "number", value });
+      continue;
+    }
+
+    if (char === '"') {
+      let value = "";
+      char = input[++current];
+      while (char !== '"') {
+        value += char;
+        char = input[++current];
+      }
+      char = input[++current];
+      tokens.push({ type: "string", value });
+      continue;
+    }
+
+    let LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)) {
+      let value = "";
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+      tokens.push({ type: "name", value });
+      continue;
+    }
+    throw new TypeError("I dont know what this character is: " + char);
   }
+  return tokens;
 }
 
 // Perché è fondamentale avere un parser in un compilatore? Quale ruolo svolge?
